648d66ccd5262dc9c3aca24af27221fd4f1535b6
Lucero Dn <lucero_dn@hotmail.es>
2019-03-21 10:05:33 -0600

    Se agregaron funciones de ingreso y registro en main.js

--- src/data.js
+++ src/data.js
@@ -1,3 +1,15 @@
+(function(){
+    // Initialize Firebase
+    var config = {
+      apiKey: "AIzaSyC1aHe9K1Pe3uvV_tBEogDpUMv8pOu-Uys",
+      authDomain: "vxm-viaja-por-mexico-d1684.firebaseapp.com",
+      databaseURL: "https://vxm-viaja-por-mexico-d1684.firebaseio.com",
+      projectId: "vxm-viaja-por-mexico-d1684",
+      storageBucket: "vxm-viaja-por-mexico-d1684.appspot.com",
+      messagingSenderId: "19267919097"
+    };
+    firebase.initializeApp(config);
+} ());
 
 // window.data = {
 //   key: 10784520,
