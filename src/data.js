0292e30451f594b1ff3d0a43ee125c6bde1bd250
LizGarciaC <lunaobel@hotmail.com>
2019-08-01 15:54:52 -0500

    Se actualizan datos sobre autenticación

---
+++ src/data.js
@@ -0,0 +1,14 @@
+
+    // Initialize Firebase
+    var config = {
+        apiKey: "AIzaSyA8shlOmuzRDiVajqFG000KfimiBLJymxs",
+        authDomain: "vxm-viaja-por-mexico.firebaseapp.com",
+        databaseURL: "https://vxm-viaja-por-mexico.firebaseio.com",
+        projectId: "vxm-viaja-por-mexico",
+        storageBucket: "vxm-viaja-por-mexico.appspot.com",
+        messagingSenderId: "732082116963"
+    };
+    firebase.initializeApp(config);
+ 
+
+ 
